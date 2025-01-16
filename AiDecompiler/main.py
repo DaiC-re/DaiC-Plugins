71584aaf4b80a5e9eec351e1bad115ac2058ea5b
Tipbs <thibaut.degodenne@epitech.eu>
2025-01-17 02:12:41 +0800

    Add get_valid_list method to get register plugin Add interaction module to prompt thing to user Add pybind11 fetching to root cmake

--- AiDecompiler/main.py
+++ AiDecompiler/main.py
@@ -2,19 +2,21 @@ from llama_cpp import Llama
 import DaiCRegister
 
 def ai_generator(bv):
-    model_path=input("model path = ")
+    model_path=DaiCRegister.get_str()
     llm = Llama(
           model_path,
           # n_gpu_layers=-1, # Uncomment to use GPU acceleration
           # seed=1337, # Uncomment to set a specific seed
           # n_ctx=2048, # Uncomment to increase the context window
     )
+    input = DaiCRegister.get_str()
     output = llm(
-          "Q: Name the planets in the solar system? A: ", # Prompt
+            input,
           max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
           stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
           echo=True # Echo the prompt back in the output
     ) # Generate a completion, can also call create_completion
     print(output)
-
-DaiCRegister.register(ai_generator)
+DaiCRegister.register("AiDecompiler", "call decompiler model with llama_cpp", ai_generator)
+a = DaiCRegister.get_valid_list()
+# a[2](1)
