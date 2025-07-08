ee4009786b781a61bd62b1f4b005ab5204d61993
Tipbs <thibaut.degodenne@epitech.eu>
2025-07-08 15:11:45 +0200

    Add some debug line for presentation

--- Example/__init__.py
+++ Example/__init__.py
@@ -1,13 +1,14 @@
 import DaiCCore
 
 def example():
-    # DaiCCore.add_to_menu("test")
-    # def print_test():
-    #     print("test toolBar")
-    # DaiCCore.add_to_tool("test2", print_test)
-    # file = DaiCCore.get_file()
-    print("Executing example")
-    pass
+    DaiCCore.add_to_menu("test")
+    def print_test():
+        print("test toolBar")
+    DaiCCore.add_to_tool("test2", print_test)
+    file = DaiCCore.get_file()
+    print (file)
+    imports = DaiCCore.get_imports()
+    print(imports)
+
 
 DaiCCore.register("Example", "Example plugin", example)
-# a = DaiCCore.get_valid_list()
