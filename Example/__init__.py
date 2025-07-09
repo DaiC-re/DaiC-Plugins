648d2a9a20aff9c21e8e4cfa8292e23077e6ba1b
Tipbs <thibaut.degodenne@epitech.eu>
2025-07-10 01:30:35 +0200

    Fix compilation issue

--- Example/__init__.py
+++ Example/__init__.py
@@ -1,12 +1,12 @@
 import DaiCCore
 
 def example():
-    DaiCCore.add_to_menu("test")
-    def print_test():
-        print("test toolBar")
-    DaiCCore.add_to_tool("test2", print_test)
-    file = DaiCCore.get_file()
-    print (file)
+    # DaiCCore.add_to_menu("test")
+    # def print_test():
+    #     print("test toolBar")
+    # DaiCCore.add_to_tool("test2", print_test)
+    # file = DaiCCore.get_file()
+    # print (file)
     imports = DaiCCore.get_imports()
     print(imports)
 
