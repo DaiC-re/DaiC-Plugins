0e921ce8494d6324d907ce807aad5477bf884b99
Tipbs <thibaut.degodenne@epitech.eu>
2025-10-12 14:20:17 +0200

    Update Example plugin with new plugin loading system

--- Example/__init__.py
+++ Example/__init__.py
@@ -1,17 +1,25 @@
-import DaiCCore
+import DaiCCore  # this is your pybind11 module that exposes Plugin
+import sys
 
-def example():
-    # DaiCCore.add_to_menu("test")
-    # def print_test():
-    #     print("test toolBar")
-    # DaiCCore.add_to_tool("test2", print_test)
-    # file = DaiCCore.get_file()
-    # print (file)
-    # imports = DaiCCore.get_imports()
-    # print(imports)
-    # def f(a):
-    #     print(a)
-    # DaiCCore.connect_functions_table(f)
-    print("Example plugin")
 
-DaiCCore.register("Example", "Example plugin", example)
+class Example(DaiCCore.Plugin):
+    name = "InfoPrinter"
+    description = "A plugin that prints all its metadata and current path"
+    version = "1.0.0"
+    author = "DaiC Team"
+
+    def init(self):
+        print(f"[{self.name}] Initialized successfully!")
+        
+
+    def run(self):
+        print("--- Plugin Information ---")
+        print(f"Name:        {self.name}")
+        print(f"Version:     {self.version}")
+        print(f"Description: {self.description}")
+        print(f"Author:      {getattr(self, 'author', 'Unknown')}")
+        print(f"Path:        {__file__}")
+        print("---------------------------")
+
+    def terminate(self):
+        print(f"[{self.name}] Terminated cleanly.")
